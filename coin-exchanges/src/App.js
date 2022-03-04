96bf70e44728611109bfc590683b7af5951fc94e
davidB2ya <dbedoya0408@gmail.com>
2022-03-04 14:39:33 -0500

    first commit

---
+++ coin-exchanges/src/App.js
@@ -0,0 +1,12 @@
+import SocialCard from './components/SocialCard';
+import './sass/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <SocialCard/>
+    </div>
+  );
+}
+
+export default App;
